d889f0fdd3084c2cbc0f666dbb8f01547038b7bd
Yashraj221b <97579137+YashrajGhule@users.noreply.github.com>
2022-12-19 21:02:08 +0530

    Add project files.

---
+++ LaptopTimer/KeyboardHook.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+namespace LaptopTimer
+{
+    internal class KeyboardHook
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct KBDLLHOOKSTRUCT
+        {
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr KeyboardHookProc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            // Check if the hook code is HC_ACTION (i.e. a keyboard event has occurred)
+            if (nCode == 0)
+            {
+
+                // Check if the key was pressed (i.e. wParam == WM_KEYDOWN)
+                if (wParam == (IntPtr)0x0100)
+                {
+                    // Cast the lParam parameter to a KBDLLHOOKSTRUCT structure
+                    KBDLLHOOKSTRUCT keyboardHookStruct = (KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(KBDLLHOOKSTRUCT));
+                    return 1;
+                }
+            }
+
+            // Call the next hook in the chain
+            return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+        }
+
+    }
+}
---
+++ LaptopTimer/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Threading;
+
+namespace LaptopTimer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int time { get; set; } = 0;
+        private int threadActive { get; set; } = 1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Hide();
+            Thread thread = new Thread(new ThreadStart(timerThread));
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+        }
+
+        private void SHUTDOWN_BUTTON_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you want to shutdown system", "WARNING", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                Process.Start("shutdown", "/s /t 0");
+            }
+            threadActive = 1;
+        }
+
+        private void CONTINUE_BUTTON_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            time = 0;
+            threadActive = 1;
+        }
+
+        private void timerThread()
+        {
+            threadActive = 1;
+            while (true)
+            {
+                Thread.Sleep(10000);
+                if (threadActive == 1)
+                {
+                    if (time >= 60)
+                    {
+                        this.Dispatcher.Invoke(() => { this.Show(); });
+                        threadActive = 0;
+                    }
+                    time += 10;
+                }
+            }
+        }
+    }
+}
